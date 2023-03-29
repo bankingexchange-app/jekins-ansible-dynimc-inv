6de876379127b2919193d13a55c03b81373cd3c2
Manju N <77180244+manju551609@users.noreply.github.com>
2023-03-29 12:24:58 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "ap-southeast-2"
 }
 
-resource "aws_instance" "AWSInstances" {
+resource "aws-instance" "AWSInstances" {
      ami = "ami-0cbf852f55ea404a6"
 	 instance_type = "t2.micro"
 	 key_name = "devops"
